db56100c0fa96e2264924bba5aeb83be593910c2
曹远鹏 <caoyp@guahao.com>
2020-05-30 19:07:33 +0800

    fix:修改路由引入方式

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,12 @@
  * @Author: caoyp
  * @Date: 2020-03-17 20:52:54
  * @Last Modified by: caoyp
- * @Last Modified time: 2020-05-30 19:04:14
+ * @Last Modified time: 2020-05-30 19:07:18
  * @Description: Description
  * @Route: Route
  */
 const Template = () => import(/* webpackChunkName: "index" */ '@/pages/index');
-// const Detail = () => import(/* webpackChunkName: "detail" */ '@/pages/detail');
+const Detail = () => import(/* webpackChunkName: "detail" */ '@/pages/test');
 
 const routes = [
     {
@@ -17,6 +17,14 @@ const routes = [
             title: '首页'
         }
     },
+    {
+        path: '/detail',
+        component: Detail,
+        meta: {
+            title: '详情'
+        }
+    },
+    
 ];
 
 export default routes;
