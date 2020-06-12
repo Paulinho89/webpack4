20b13d1f9f77da8a412928bf9b07dc2e82fe5e76
曹远鹏 <caoyp@guahao.com>
2020-06-12 17:13:52 +0800

    upd:ceshi1

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,7 @@
  * @Author: caoyp
  * @Date: 2020-03-15 17:11:59
  * @Last Modified by: caoyp
- * @Last Modified time: 2020-05-31 18:21:43
+ * @Last Modified time: 2020-06-12 17:13:23
  * @Description: Description
  * @Route: Route
  */
@@ -16,7 +16,7 @@
 export default {
     data() {
         return {
-            testData: 'Welcome to jenkins自动化部署测试文档编写'
+            testData: 'Welcome to jenkins自动化部署测试11111'
         };
     }
 };
